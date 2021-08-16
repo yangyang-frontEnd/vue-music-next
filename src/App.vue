08e9bb5c341e80c9ddccc97f9c76f50bf95f11d4
yangyang <1434753182@qq.com>
2021-08-16 18:08:20 +0800

    Complete the project basic code writing.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <m-header></m-header>
+</template>
+
+<script>
+import Header from '@/components/header/header.vue'
+
+export default {
+    name: 'App',
+    components: {
+        MHeader: Header
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
